75b77d2135b3844a4d4283b2748b30bc9b779f6d
Matt Ford <mateoford@gmail.com>
2018-10-28 20:41:16 -0500

    Add basic Forces pane to tool panel

--- wheel_app.py
+++ wheel_app.py
@@ -1,7 +1,7 @@
 from bokeh.io import output_file, show
 from bokeh.layouts import column, row, widgetbox
 from bokeh.plotting import figure
-from bokeh.models.widgets import Button, RadioButtonGroup, Select, Slider, Paragraph, Div, TextInput
+from bokeh.models.widgets import Button, RadioButtonGroup, Select, Slider, Paragraph, Div, TextInput, Panel, Tabs
 
 # Define output file
 output_file('layout_test.html')
@@ -26,22 +26,31 @@ hub_diam = Slider(title='Flange diameter [mm]', start=10, end=80, step=1, value=
 hub_offset = Slider(title='Dish offset [mm]', start=-30, end=30, step=1, value=0)
 
 # Create spoke controls
-spk_matl = RadioButtonGroup(labels=['Alloy', 'Steel', 'Carbon'], active=0)
+spk_matl = RadioButtonGroup(labels=['Steel', 'Alloy', 'Carbon'], active=0)
 spk_num = Slider(title='Number of spokes', start=8, end=64, step=4, value=36)
 spk_diam = Slider(title='Diameter [mm]', start=1., end=3., step=0.1, value=2.)
 spk_tension = Slider(title='Average spoke tension [kgf]', start=0., end=200, step=2, value=100)
 spk_pattern = Select(title='Spoke pattern', value='3-cross',
                      options=['Radial', '1-cross', '2-cross', '3-cross', '4-cross'])
 
-# Widget layout
-panel = widgetbox(Div(text='<strong>Rim</strong>'),
-                  rim_matl, rim_size, rim_mass,
-                  rim_EI1, rim_EI2, rim_GJ,
-                  Div(text='<hr/><strong>Hub</strong>'),
-                  hub_width, hub_diam, hub_offset,
-                  Div(text='<hr/><strong>Spokes</strong>'),
-                  spk_matl, spk_num, spk_diam, spk_tension, spk_pattern)
+wheel_pane = widgetbox(Div(text='<strong>Rim</strong>'),
+                       rim_matl, rim_size, rim_mass,
+                       rim_EI1, rim_EI2, rim_GJ,
+                       Div(text='<hr/><strong>Hub</strong>'),
+                       hub_width, hub_diam, hub_offset,
+                       Div(text='<hr/><strong>Spokes</strong>'),
+                       spk_matl, spk_num, spk_diam, spk_tension, spk_pattern)
 
+# Forces pane
+f1_dof = RadioButtonGroup(labels=['Radial', 'Lateral', 'Tangential'], active=0)
+f1_loc = TextInput(title='Location [degrees]:', value='0')
+f1_mag = TextInput(title='Magnitude [N]:', value='0')
+force_pane = widgetbox(Div(text='<strong>Forces</strong>'),
+                       f1_dof, f1_loc, f1_mag)
+
+# Combine Wheelbuilding and Forces pane
+tool_panel = Tabs(tabs=[Panel(child=wheel_pane, title='Wheel'),
+                        Panel(child=force_pane, title='Forces')])
 
 # Result plots
 p1 = figure(plot_height=250)
@@ -49,6 +58,5 @@ p2 = figure(plot_height=250)
 
 
 # Render the document
-show(column(row(Div(text='One'), Button(label='Update Results', button_type='success')),
-            row(panel, column(p1, p2))))
-
+show(row(tool_panel, column(Button(label='Update Results', button_type='success'),
+                             p1, p2)))
