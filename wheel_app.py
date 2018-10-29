7fd4fbb7987cefefec4722c3f38bca0d01a10275
Matt Ford <mateoford@gmail.com>
2018-10-29 10:17:58 -0500

    Organize wheel options and forces into tool panes

--- wheel_app.py
+++ wheel_app.py
@@ -26,11 +26,15 @@ def build_wheel_from_UI():
                 I11=25./26e9, I22=200./69e9, I33=100./69e9, Iw=0.0,
                 young_mod=69e9, shear_mod=26e9)
 
+
+
     w.lace_cross(n_spokes=36, n_cross=n_cross, diameter=1.8e-3, young_mod=210e9, offset=0.)
 
 
 # Define output file
 output_file('layout_test.html')
+
+# Create presets controls
 
 
 # Create rim controls
@@ -46,10 +50,17 @@ rim_EI2 = Slider(title='Lateral stiffness [N m^2]',
 rim_GJ = Slider(title='Torsional stiffness [N m^2]',
                 start=10, end=500, value=100, step=10)
 
+rim_pane = widgetbox(Div(text='<strong>Rim</strong>'),
+                     rim_matl, rim_size, rim_mass,
+                     rim_EI1, rim_EI2, rim_GJ)
+
 # Create hub controls
 hub_width = Slider(title='Flange separation [mm]', start=10, end=80, step=1, value=50)
 hub_diam = Slider(title='Flange diameter [mm]', start=10, end=80, step=1, value=50)
 hub_offset = Slider(title='Dish offset [mm]', start=-30, end=30, step=1, value=0)
+
+hub_pane = widgetbox(Div(text='<hr/><strong>Hub</strong>'),
+                     hub_width, hub_diam, hub_offset)
 
 # Create spoke controls
 spk_matl = RadioButtonGroup(labels=['Steel', 'Alloy', 'Carbon'], active=0)
@@ -59,13 +70,8 @@ spk_tension = Slider(title='Average spoke tension [kgf]', start=0., end=200, ste
 spk_pattern = Select(title='Spoke pattern', value='3-cross',
                      options=['Radial', '1-cross', '2-cross', '3-cross', '4-cross'])
 
-wheel_pane = widgetbox(Div(text='<strong>Rim</strong>'),
-                       rim_matl, rim_size, rim_mass,
-                       rim_EI1, rim_EI2, rim_GJ,
-                       Div(text='<hr/><strong>Hub</strong>'),
-                       hub_width, hub_diam, hub_offset,
-                       Div(text='<hr/><strong>Spokes</strong>'),
-                       spk_matl, spk_num, spk_diam, spk_tension, spk_pattern)
+spk_pane = widgetbox(Div(text='<hr/><strong>Spokes</strong>'),
+                     spk_matl, spk_num, spk_diam, spk_tension, spk_pattern)
 
 # Forces pane
 f1_dof = RadioButtonGroup(labels=['Radial', 'Lateral', 'Tangential'], active=0)
@@ -75,8 +81,12 @@ force_pane = widgetbox(Div(text='<strong>Forces</strong>'),
                        f1_dof, f1_loc, f1_mag)
 
 # Combine Wheelbuilding and Forces pane
-tool_panel = Tabs(tabs=[Panel(child=wheel_pane, title='Wheel'),
+tool_panel = Tabs(tabs=[Panel(child=rim_pane, title='Rim'),
+                        Panel(child=hub_pane, title='Hub'),
+                        Panel(child=spk_pane, title='Spokes'),
                         Panel(child=force_pane, title='Forces')])
+
+
 
 # Result plots
 p1 = figure(plot_height=250)
